f69608efa258b750aa33050554a3cba2619c55ee
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-03-17 15:24:27 -0700

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(task = "roller-skate"){    
+    return `This Saturday, I want to ${task}!`; 
+} 
+saturdayFun();
+
+
+function mondayWork(task = "go to the office"){
+    return `This Monday, I will ${task}.`;
+}
+mondayWork();
+
+
+function wrapAdjective(specialChar){
+    
+    return function specialMsg(message) {
+    
+return `You are ${specialChar}${message}${specialChar}!`;
+    }
+    
+}
+wrapAdjective("*",specialMsg("a hard worker"))
